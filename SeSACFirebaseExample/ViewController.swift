971fe22ee28e52b3f516c730a838256e06791c81
minhye9731 <jenney62639731@gmail.com>
2022-10-05 10:29:41 +0900

    Initial Commit

---
+++ SeSACFirebaseExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACFirebaseExample
+//
+//  Created by 강민혜 on 10/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
